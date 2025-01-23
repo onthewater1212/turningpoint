9128112ca990d3d878e9ba151f25185e10108912
onthewater1212 <moe.mizukami.8i@stu.hosei.ac.jp>
2025-01-23 21:24:51 +0900

    mokuji

--- js/functions.js
+++ js/functions.js
@@ -60,37 +60,42 @@ const endValue = isMobile ? "+=500" : "+=700"; // スマホなら500、それ以
     },
   });
 
+  window.addEventListener('load', () => {
   gsap.utils.toArray('.hidden-text').forEach((element) => {
     gsap.fromTo(
-      element,
+      element,// 対象
       { opacity: 0, translateY: 50 }, // 初期状態（透明で下にずれている）
-      { 
+      { //変化後状態
         opacity: 1, 
-        translateY: 0, 
-        duration: 1, 
-        ease: 'power2.out', 
+        translateY: 0, //元の位置に戻る
+        duration: 1, //アニメーション時間
+        ease: 'power2.out', //イージング
         scrollTrigger: {
           trigger: element, // アニメーションを開始するトリガー要素
           start: 'top 90%', // 要素の上端がビューポートの90%地点に来たら開始
-          end: 'top 60%', // 必要ならアニメーション範囲を指定
+          end: 'top 60%', 
           toggleActions: 'play none none reverse', // アニメーションの挙動
+          markers: true,
         },
       }
     );
-  });
+});
+
+ScrollTrigger.refresh();
+});
   
     //配列のインデックスは0から始まる
 
 // script.js
 document.addEventListener("DOMContentLoaded", () => {
-  const toc = document.getElementById("table-of-contents");
-  const tocOffsetTop = toc.offsetTop;
+  const toc = document.getElementById("table-of-contents");//変数に格納
+  const tocOffsetTop = toc.offsetTop;//toc 要素の 親要素の上端からの距離を取得
 
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > tocOffsetTop) {
-      toc.classList.add("fixed");
+  window.addEventListener("scroll", () => {//スクロールのたびに実行される
+    if (window.scrollY > tocOffsetTop) {//現在のスクロール位置（ページの最上部からのピクセル数）を取得
+      toc.classList.add("fixed");//固定
     } else {
-      toc.classList.remove("fixed");
+      toc.classList.remove("fixed");//固定を削除
     }
   });
 });
