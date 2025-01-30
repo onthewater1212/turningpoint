5791e5ee683ccb1bdeeb21afc8b22658564a2527
onthewater1212 <moe.mizukami.8i@stu.hosei.ac.jp>
2025-01-30 17:50:42 +0900

    ｋ

--- js/functions.js
+++ js/functions.js
@@ -1,85 +1,86 @@
 
-
-  document.querySelector(".scroll-image").addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth", // スムーズにスクロール
-    });
+//topへをクリックしたら一番上に
+document.querySelector(".scroll-image").addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // スムーズにスクロール
   });
+});
 
-  document.addEventListener("scroll", function() {
-    let scrollContainer = document.querySelector(".scroll-container");
-    let triggerHeight = 800; // どの高さから固定するか
+// topへを途中から固定
+document.addEventListener("scroll", function() {
+  let scrollContainer = document.querySelector(".scroll-container");
+  let triggerHeight = 2500; // どの高さから固定するか
 
-    if (window.scrollY > triggerHeight) {
-        scrollContainer.classList.add("fixed");
-    } else {
-        scrollContainer.classList.remove("fixed");
-    }
+  if (window.scrollY > triggerHeight) {
+      scrollContainer.classList.add("fixed");
+  } else {
+      scrollContainer.classList.remove("fixed");
+  }
 });
   
-  $(document).ready(function() {
-      $('#wave, #wave-bottom').wavify({
-      height: 100,
-      bones: 10,//波の山と谷の数
-      amplitude: 20,//振幅
-      color: '#2731AD',
-      speed: .25//速度
-    });
+//波
+$(document).ready(function() {
+    $('#wave, #wave-bottom').wavify({
+    height: 100,
+    bones: 10,//波の山と谷の数
+    amplitude: 20,//振幅
+    color: '#2731AD',
+    speed: .25//速度
   });
-  
-  
-  const images = [
-    "/images/yacht1.png",
-    "/images/yacht2.png",
-    "/images/yacht3.png",
-    "/images/yacht4.png",
-    "/images/yacht5.png",
-    "/images/yacht6.png",
-    "/images/yacht7.png",
-    "/images/yacht8.png",
-    "/images/yacht9.png",
-    "/images/yacht10.png",
-  ];
+});
 
-  const images2 = [
-  "/images/vs1.png",
-  "/images/vs2.png",
-  "/images/vs3.png",
-  "/images/vs4.png",
-  "/images/vs5.png",
-  "/images/vs6.png",
-  "/images/vs7.png",
-  "/images/vs8.png",
-  "/images/vs9.png",
-  "/images/vs10.png",
-  ];
-  
+
   // 画像要素
+const images = [
+  "/images/yacht1.png",
+  "/images/yacht2.png",
+  "/images/yacht3.png",
+  "/images/yacht4.png",
+  "/images/yacht5.png",
+  "/images/yacht6.png",
+  "/images/yacht7.png",
+  "/images/yacht8.png",
+  "/images/yacht9.png",
+  "/images/yacht10.png",
+];
+
+const images2 = [
+"/images/vs1.png",
+"/images/vs2.png",
+"/images/vs3.png",
+"/images/vs4.png",
+"/images/vs5.png",
+"/images/vs6.png",
+"/images/vs7.png",
+"/images/vs8.png",
+"/images/vs9.png",
+"/images/vs10.png",
+];
+//配列のインデックスは0から始まる
  
   const yacht = document.getElementById("yacht");
   const vs = document.getElementById("vs"); 
   const totalFrames = images.length; // フレームの数
 
-// デバイスによってendの値を動的に設定
-const isMobile = window.matchMedia("(max-width: 768px)").matches;
-const endValue = isMobile ? "+=500" : "+=700"; // スマホなら500、それ以外なら700
 
 if (yacht) {
   yacht.src = images[0];
 }
 
+
     // GSAPとScrollTriggerを使ってアニメーションを設定
   gsap.registerPlugin(ScrollTrigger);
+
  // vsのアニメーショn
   gsap.to({}, {// アニメーションさせる要素は特定じゃないからなし
     scrollTrigger: {//進行状況を監視する役割
       trigger: ".vs-container", // トリガーとなる要素
       start: "center center", //animation-containerのtopがブラウザのcenterに来たらピン留め
-      end: endValue, // ピン留め範囲
+      end: "+=700", // ピン留め範囲
       scrub: true, // スクロールに連動させる
       pin: true, // トリガー要素のピン留めを有効にする
-      markers: false,
+      markers: true,
      //srcを切り替え、スクロールするごとに呼び出される関数
       onUpdate: (self) => {//selfはScrolltriggerのこと
         console.log("ScrollTrigger progress:", self.progress); // 進行状況を確認
@@ -88,6 +89,8 @@ if (yacht) {
           totalFrames - 1,// 計算結果が配列の範囲を越えないように制限
           Math.floor(progress * totalFrames)//小数点以下切り捨て進行度に応じたフレーム番号を計算
         );
+      
+        console.log(`Frame index: ${frameIndex}`);
         if (vs) {
           vs.src = images2[frameIndex];
           console.log(`VS src updated to: ${images2[frameIndex]}`);
@@ -102,11 +105,11 @@ if (yacht) {
     scrollTrigger: {//進行状況を監視する役割
       trigger: ".yacht-container", // トリガーとなる要素
       start: "center center", //animation-containerのtopがブラウザのcenterに来たらピン留め
-      end: endValue, // ピン留め範囲
+      end: "+=700", // ピン留め範囲
       scrub: true, // スクロールに連動させる
       pin: true, // トリガー要素のピン留めを有効にする
       pinSpacer: false, // pin-spacerの生成を防ぐ
-      markers: true,
+      markers: false,
      //srcを切り替え、スクロールするごとに呼び出される関数
       onUpdate: (self) => {//selfはScrolltriggerのこと
         console.log("ScrollTrigger progress:", self.progress); // 進行状況を確認
@@ -123,44 +126,46 @@ if (yacht) {
     },
   });
 
-  // vsのアニメーショ
+// 目次
+gsap.to(".table-of-contents", {
+  scrollTrigger: {
+    trigger: ".table-of-contents", // トリガーとなる要素
+    start: "top top", // トリガー開始位置
+    endTrigger: ".interview",
+    end: "bottom top", // トリガー終了位置
+    pin: true, // 要素を固定する
+    scrub: false, // スクロールに連動する
+  },
+});
 
-  gsap.to(".table-of-contents", {
-    scrollTrigger: {
-      trigger: ".table-of-contents", // トリガーとなる要素
-      start: "top top", // トリガー開始位置
-      endTrigger: ".interview",
-      end: "bottom top", // トリガー終了位置
-      pin: true, // 要素を固定する
-      scrub: false, // スクロールに連動する
-    },
-  });
-  
 
-  //文字をふわっとだす
-  window.addEventListener('load', () => {
+//インタビュー内容をふわっとだす
+window.addEventListener('load', () => {
+  if(window.innerWidth > 768) {
   gsap.utils.toArray('.hidden-text').forEach((element) => {
-    gsap.fromTo(
-      element,// 対象
-      { opacity: 0, translateY: 50 }, // 初期状態（透明で下にずれている）
-      { //変化後状態
-        opacity: 1, 
-        translateY: 0, //元の位置に戻る
-        duration: 1, //アニメーション時間
-        ease: 'power2.out', //イージング
-        scrollTrigger: {
-          trigger: element, // アニメーションを開始するトリガー要素
-          start: 'top 90%', // 要素の上端がビューポートの90%地点に来たら開始
-          end: 'top 60%', 
-          toggleActions: 'play none none reverse', // アニメーションの挙動
-          markers: false,
-        },
-      }
-    );
+  gsap.fromTo(
+    element,// 対象
+    { opacity: 0, translateY: 50 }, // 初期状態（透明で下にずれている）
+    { //変化後状態
+      opacity: 1, 
+      translateY: 0, //元の位置に戻る
+      duration: 1, //アニメーション時間
+      ease: 'power2.out', //イージング
+      scrollTrigger: {
+        trigger: element, // アニメーションを開始するトリガー要素
+        start: 'top 90%', // 要素の上端がビューポートの90%地点に来たら開始
+        end: 'top 60%', 
+        toggleActions: 'play none none reverse', // アニメーションの挙動
+        markers: false,
+      },
+    }
+  );
 });
 
-ScrollTrigger.refresh();
+    ScrollTrigger.refresh();
+  }
 });
+
+
   
-    //配列のインデックスは0から始まる
-//目次固定
+    
